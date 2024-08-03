eafef2a0f5e62655c5ba9927a96eac1e1a19d97e
Jesutofunmi enoch <enochagram81@gmail.com>
2024-08-03 17:55:33 +0100

    changes

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -8,22 +8,27 @@ import {
     RoomProvider,
     ClientSideSuspense,
   } from "@liveblocks/react/suspense";
+import ActiveCollaborators from './ActiveCollaborators';
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           <div className="collaborative-room">
           <Header className="sticky left-0 top-0">
-        <div className="flex items-center gap-2 lg:gap-4">
-          <p className='document-title'>This is a fake test</p>
+        <div className="flex w-fit items-center gap-2 justify-center">
+          <p className='document-title'>Share</p>
+          </div>
+          <div className="flex w-full flex-1 justify-end gap-2 lg:gap-3">
+            <ActiveCollaborators/>
+          
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
-        </div>
+          </div>
       </Header>
       <Editor/>
           </div>
